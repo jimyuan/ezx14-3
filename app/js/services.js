16a01a6371a18228cf7788c60fe7da9f8b2b1e5d
Jim Yuan <aquarior@gmail.com>
2014-09-10 18:02:19 +0800

    phase-1 modify production path evn

--- app/js/services.js
+++ app/js/services.js
@@ -1,8 +1,11 @@
 (function(){
   var app = angular.module('FacebookServices', []);
 
-  app.factory('Students', ['$resource', function($resource){
+  app.factory('Students', ['$resource', '$window', function($resource, $window){
     var url='/json/:service.json';
+    if ($window.location.host.indexOf('github.io') !== -1){
+      url = '/app' + url;
+    }
     return $resource(url, {}, {
       query:{
         params:{service: 'students'}
